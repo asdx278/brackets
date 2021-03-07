9e3d15c117f250d22d6443b78c995e8cbdbe4313
asdx278 <albert.shubin@gmail.com>
2021-03-07 16:47:44 +0400

    feat: update undex

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 == 1) {
+        return false;
+    }
+    let arr = new Array();
+    for (let i = 0; i <= str.length - 1; i++) {
+        let find = false;
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0]) {
+                find = true;
+                if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+                    if (arr[arr.length - 1] == bracketsConfig[j][0]) {
+                        arr.pop();
+                        break;
+                    }
+                }
+                arr.push(bracketsConfig[j][1]);
+                break;
+            }
+        }
+        if (!find) {
+            if (arr.length == 0 || str[i] != arr[arr.length - 1]) {
+                return false;
+            }
+            arr.pop();
+        }
+    }
+    return arr.length == 0 ? true : false;
 }
